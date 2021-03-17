d91b9e219262f99e7ef40c55376259615915a9c7
Craig MacLeod <c92macleod@gmail.com>
2021-03-17 16:36:39 +0100

    linked the horoscope url to the DOM

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
+const form = document.querySelector("form");
 
-const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
+
+form.addEventListener("submit", (event) => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData)
+    const sign = data.sign
+    const day = data.day
+    const URL = `https://aztro.sameerkumar.website/?sign=${sign}&day=${day}`;
+//    const URL2 = ``
+    event.preventDefault();
 fetch(URL, {
-    method: 'POST'
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { "content-type": "application/json" },
 })
-.then(response => response.json())
-.then(json => {
-    const date = json.current_date;
-    console.log(date);
+.then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json()
+})
+.then(json => { 
+    document.getElementById("testp").innerHTML = `Your lucky time ${day} is ${json.lucky_time}, and your lucky number is ${json.lucky_number}`
+    console.log(json.lucky_time);
+    console.log(json.lucky_number);
+    console.log(json.mood);
+    console.log(json.color);
+    console.log(json.compatibility);
+    console.log(json.description);
+    })
+    .catch((error) => console.error(error));
+// Second fetch will go here
 });
