e43d806be049fdc0e7dedacf675f1a2a59291b7a
Craig MacLeod <c92macleod@gmail.com>
2021-03-18 12:14:10 +0100

    scripted DOB to starsign function

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 
 const form = document.querySelector("form");
 
+// Find starsign from Date of Birth user input
+function starsign(dob) {
+    const dobDay = Number(dob.split('-')[2])
+    const dobMonth = Number(dob.split('-')[1])
+    if ((dobDay >= 21 && dobMonth === 3) || (dobDay <= 19 && dobMonth === 4)) {
+        return "aries"
+    }
+    if ((dobDay >= 20 && dobMonth === 4) || (dobDay <= 20 && dobMonth === 5)) {
+        return "taurus"
+    }
+    if ((dobDay >= 21 && dobMonth === 5) || (dobDay <= 20 && dobMonth === 6)) {
+        return "gemini"
+    }
+    if ((dobDay >= 21 && dobMonth === 6) || (dobDay <= 22 && dobMonth === 7)) {
+        return "cancer"
+    }
+    if ((dobDay >= 23 && dobMonth === 7) || (dobDay <= 22 && dobMonth === 8)) {
+        return "leo"
+    }
+    if ((dobDay >= 23 && dobMonth === 8) || (dobDay <= 22 && dobMonth === 9)) {
+        return "virgo"
+    }
+    if ((dobDay >= 23 && dobMonth === 9) || (dobDay <= 22 && dobMonth === 10)) {
+        return "libra"
+    }
+    if ((dobDay >= 23 && dobMonth === 10) || (dobDay <= 22 && dobMonth === 11)) {
+        return "scorpio"
+    }
+    if ((dobDay >= 23 && dobMonth === 11) || (dobDay <= 21 && dobMonth === 12)) {
+        return "sagittarius"
+    }
+    if ((dobDay >= 22 && dobMonth === 12) || (dobDay <= 19 && dobMonth === 1)) {
+        return "capricorn"
+    }
+    if ((dobDay >= 20 && dobMonth === 1) || (dobDay <= 18 && dobMonth === 2)) {
+        return "aquarius"
+    }
+    if ((dobDay >= 19 && dobMonth === 2) || (dobDay <= 20 && dobMonth === 3)) {
+        return "pisces"
+    }
+    else {
+        console.log("you do not exist")
+    }
+}
 
+// Fetch data from API
 form.addEventListener("submit", (event) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData)
-    const sign = data.sign
+    const sign = starsign(data.sign)
     const day = data.day
     const URL = `https://aztro.sameerkumar.website/?sign=${sign}&day=${day}`;
 //    const URL2 = ``
